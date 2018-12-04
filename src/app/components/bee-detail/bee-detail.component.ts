567037687e67bc5902c18c0f57a73d4906483b5e
Curtis Schlak <170594+realistschuckle@users.noreply.github.com>
2018-12-04 13:18:40 -0500

    Exercises 1 & 2

---
+++ src/app/components/bee-detail/bee-detail.component.ts
@@ -0,0 +1,28 @@
+import {
+  Component, OnInit
+} from '@angular/core';
+import { Bee } from '../../models/bee';
+
+@Component({
+  selector: 'app-bee-detail',
+  templateUrl: './bee-detail.component.html',
+  styleUrls: ['./bee-detail.component.scss']
+})
+export class BeeDetailComponent implements OnInit {
+  public bee: Bee;
+
+  public ngOnInit(): void {
+    this.bee = {
+      name: 'Sweat Bee',
+      description: 'Ew. Gross.',
+      genus: 'Perspirus',
+      species: 'Smellius',
+      deadly: false,
+      alias: 'The Stinky Green Machine',
+      communal: true,
+      image_url: 'https://upload.wikimedia.org/wikipedia/commons/b/b5/Iridescent.green.sweat.bee1.jpg',
+      endangered: false,
+      id: '1'
+    };
+  }
+}
